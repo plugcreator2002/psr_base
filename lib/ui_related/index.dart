0cbf160b48fa699cd2db860cacb1950ff6c4b0bd
plugcreator2002 <102593701+plugcreator2002@users.noreply.github.com>
2022-06-06 16:52:12 +0430

    Update index.dart

--- lib/ui_related/index.dart
+++ lib/ui_related/index.dart
@@ -1,7 +1,14 @@
+/// animated widgets
 export './animation/index.dart';
+
+/// show widgets (horizontal or vertical) with only one boolean variable
 export './direction_status.dart';
+
+/// blurring the loading time screen
 export './loading_page.dart';
 export './builders/index.dart';
+
+/// space in between widgets
 export './space_between.dart';
 export './dashed_border/index.dart';
 export './buttons/index.dart';
