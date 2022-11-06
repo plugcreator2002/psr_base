32615e0245fb58b8f7597507b6e3721dbeb9f059
213312123 <a.rasouli@dornica.net>
2022-11-06 09:25:06 +0330

    Signed-off-by: 213312123 <a.rasouli@dornica.net>

--- lib/utils/popups/opening_popups.dart
+++ lib/utils/popups/opening_popups.dart
@@ -4,7 +4,8 @@ import 'package:flutter/services.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
 
 class OpeningPopups {
-  static Future<T?> centerPopup<T>(final BuildContext context, {
+  static Future<T?> centerPopup<T>(
+    final BuildContext context, {
     final Widget? child,
     final Widget? desktop,
     final Widget? mobile,
@@ -21,6 +22,7 @@ class OpeningPopups {
               padding: const EdgeInsets.symmetric(vertical: 50),
               child: Center(
                 child: Material(
+                  color: Colors.transparent,
                   borderRadius: BorderRadius.circular(11),
                   child: child,
                 ),
@@ -34,7 +36,8 @@ class OpeningPopups {
     return output;
   }
 
-  static Future<T?> showBottomPopup<T>(final BuildContext context, {
+  static Future<T?> showBottomPopup<T>(
+    final BuildContext context, {
     required final Widget view,
   }) async {
     FocusScope.of(context).unfocus();
@@ -54,7 +57,7 @@ class OpeningPopups {
           ),
           builder: (context, state) {
             return Material(
-              color: Colors.white,
+              color: Colors.transparent,
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: view,
@@ -68,20 +71,19 @@ class OpeningPopups {
     return data;
   }
 
-  static void showToast(BuildContext context,
-      String message, {
-        SnackBarAction? snackBarAction,
-        EdgeInsetsGeometry? margin,
-        Color? backgroundColor,
-        Color? contentColor,
-        Duration duration = const Duration(milliseconds: 4000),
-      }) {
+  static void showToast(
+    BuildContext context,
+    String message, {
+    SnackBarAction? snackBarAction,
+    EdgeInsetsGeometry? margin,
+    Color? backgroundColor,
+    Color? contentColor,
+    Duration duration = const Duration(milliseconds: 4000),
+  }) {
     final theme = Theme.of(context);
     const defualtMargin = EdgeInsets.symmetric(horizontal: 322, vertical: 20);
 
-    ScaffoldMessenger.of(
-        context
-    ).showSnackBar(SnackBar(
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       duration: duration,
       backgroundColor: backgroundColor,
       margin: margin ?? defualtMargin,
